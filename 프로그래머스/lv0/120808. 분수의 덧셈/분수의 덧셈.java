ea8ceb2a7fbbbb2c4b2e6b0d2a52574df8d25771
DongGyu Jung <bang2brew@gmail.com>
2023-01-05 01:30:09 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int firstLCM = getLCM(num1, num2);
+        int[] answer = new int[2];
+        int finalDenum = calculate(denum1, num1, firstLCM) + calculate(denum2, num2, firstLCM);
+        int finalGCD ;
+        if(finalDenum >=firstLCM){
+            finalGCD = getGCD(finalDenum, firstLCM);
+        }else{
+            finalGCD = getGCD(firstLCM, finalDenum);
+        }
+        answer[0] = finalDenum / finalGCD;
+        answer[1] = firstLCM / finalGCD;
+        return answer;
+    }
+    private int calculate(int denum, int num, int lcm){
+        return denum * (lcm/num);
+    }
+    
+    private int getLCM(int num1, int num2){
+        if(num1 >= num2){
+            return (num1*num2) / getGCD(num1, num2);
+        }
+        return (num1*num2) / getGCD(num2, num1);
+    }
+    
+    private int getGCD(int a, int b){
+        int r ;
+        while(b != 0){ // 3, 2
+            r = a % b ;  // 1 | 0
+            a = b; // 2 | 1
+            b = r; // 1 | 0
+        }
+        return a;
+    }
+}
