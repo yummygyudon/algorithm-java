0a217d3377c62135e2e3322b549b1cf155a11dc3
DongGyu Jung <bang2brew@gmail.com>
2023-05-04 05:12:49 -0700

    [unrated] Title: 원하는 문자열 찾기, Time: 7.21 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        myString = myString.toUpperCase();
+        pat = pat.toUpperCase();
+        return myString.contains(pat) ? 1 : 0;
+    }
+}
