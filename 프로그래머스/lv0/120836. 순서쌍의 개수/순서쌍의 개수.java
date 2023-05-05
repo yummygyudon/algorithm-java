24c62116a4327790de07f5f32eae83cbbddebd6b
DongGyu Jung <bang2brew@gmail.com>
2023-05-05 01:05:57 -0700

    [level 0] Title: 순서쌍의 개수, Time: 7.57 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1;
+        while(num <= n) {
+            if(n % num == 0) {
+                answer++;
+            }
+            num++;
+        }
+        
+        return answer;
+    }
+}
