3f2e9994ed896812a016ce62a88b1110fb18a8f2
DongGyu Jung <bang2brew@gmail.com>
2023-05-08 08:32:43 -0700

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] query : queries) {
+            int tmp = arr[query[0]] ;
+            arr[query[0]] = arr[query[1]] ;
+            arr[query[1]] = tmp;
+        }
+        return arr;
+    }
+}
