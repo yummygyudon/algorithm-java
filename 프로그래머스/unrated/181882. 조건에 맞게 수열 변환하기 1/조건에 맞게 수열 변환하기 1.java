5b7d623e8b6e30e3cfc1d72c6cefe576e07ca252
DongGyu Jung <bang2brew@gmail.com>
2023-05-09 01:03:31 -0700

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.85 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        for (int i = 0 ; i < arr.length ; i ++ ){
+            if(arr[i] >= 50) {
+                if(arr[i] % 2 == 0) {
+                    arr[i] /= 2;
+                }
+            }else {
+                if(arr[i] % 2 == 1) {
+                    arr[i] *= 2;
+                }
+            }
+        }
+        return arr;
+    }
+}
