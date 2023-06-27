a5b79357bdaf8b20a70168f54aeb54adaae911c1
DongGyu Jung <bang2brew@gmail.com>
2023-06-28 01:43:16 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : str_list){
+            if(str.contains(ex)) {
+                continue;
+            }
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
