848f12c587e80ee99c9468482ec4dc5d708c6227
DongGyu Jung <bang2brew@gmail.com>
2023-06-28 14:55:51 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String[] solution(String my_string) {
+        my_string = my_string.trim();
+        String[] answer = my_string.split("[^A-z]+");
+        
+        return answer;
+    }
+}
