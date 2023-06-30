0c2d21a0e48fab0f8016dd4a5119eba36f89b44b
DongGyu Jung <bang2brew@gmail.com>
2023-06-30 19:56:55 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.43 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] result = new int[5];
+        for (int i = 0 ; i < 5; i++) {
+            result[i] = num_list[i];
+        }
+        
+        return result;
+    }
+}
