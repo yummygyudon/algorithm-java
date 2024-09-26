d7d1afb428ec79a5211a4321656140d873f510cb
DongGyu Jung <bang2brew@gmail.com>
2024-09-26 17:07:48 +0900

    [Silver V] Title: 올림픽, Time: 116 ms, Memory: 14772 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,37 +1,47 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int GOLD_VALUE = 3;
-    static int SILVER_VALUE = 2;
-    static int BRONZE_VALUE = 1;
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine(), " ");
         int N = Integer.parseInt(tokenizer.nextToken()),
                 TARGET_TEAM = Integer.parseInt(tokenizer.nextToken());
-        int tmp = 0;
-        int[][] scoreBoard = new int[N][2];
+        int[][] scoreBoard = new int[N][5];
         for (int i = 0; i < N; i++) {
             tokenizer = new StringTokenizer(reader.readLine(), " ");
-            int teamNum = Integer.parseInt(tokenizer.nextToken()),
-                    gold = Integer.parseInt(tokenizer.nextToken()),
-                    silver = Integer.parseInt(tokenizer.nextToken()),
-                    bronze = Integer.parseInt(tokenizer.nextToken());
-            int[] score = new int[2];
-
-            score[0] = teamNum;
-            int value = (gold * GOLD_VALUE) + (silver * SILVER_VALUE) + (bronze * BRONZE_VALUE);
-            score[1] = value;
-            if (teamNum == TARGET_TEAM) {
-                tmp = value;
-            }
+            int[] score = new int[5];
+            score[0] = Integer.parseInt(tokenizer.nextToken());
+            score[1] = Integer.parseInt(tokenizer.nextToken());
+            score[2] = Integer.parseInt(tokenizer.nextToken());
+            score[3] = Integer.parseInt(tokenizer.nextToken());
+            score[4] = 1; // Rank
             scoreBoard[i] = score;
         }
-        Comparator<int[]> comparator = (o1, o2) -> Integer.compare(o1[1], o2[1]);
-        Arrays.sort(scoreBoard, comparator.reversed());
 
-        int targetValue = tmp;
-        long higherCount = Arrays.stream(scoreBoard).filter(x -> x[1] > targetValue).count();
-        System.out.println(higherCount + 1);
+        Arrays.sort(scoreBoard, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                if (o1[2] == o2[2]) {
+                    return -(o1[3] - o2[3]);
+                } else {
+                    return -(o1[2] - o2[2]);
+                }
+            } else {
+                return -(o1[1] - o2[1]);
+            }
+        });
+        int targetIndex = 0;
+        for (int i = 1; i < N; i++) {
+            if (scoreBoard[i][0] == TARGET_TEAM) {
+                targetIndex = i;
+            }
+            if ((scoreBoard[i][1] == scoreBoard[i-1][1]) &&
+                    (scoreBoard[i][2] == scoreBoard[i-1][2]) &&
+                    (scoreBoard[i][3] == scoreBoard[i-1][3])) {
+                scoreBoard[i][4] = scoreBoard[i - 1][4];
+            } else {
+                scoreBoard[i][4] = i + 1; // 공동 순위 책정 때문에 이전 순서가 아닌 i로 연산
+            }
+        }
+        System.out.println(scoreBoard[targetIndex][4]);
     }
 }
