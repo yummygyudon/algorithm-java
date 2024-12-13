79ec6c0ca53edb510d7860b28c780e23ad4a12f9
DongGyu Jung <bang2brew@gmail.com>
2024-12-13 21:37:00 +0900

    [Silver I] Title: 볼 모으기, Time: 384 ms, Memory: 50704 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.java
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.java
@@ -6,28 +6,53 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int quantity = Integer.parseInt(reader.readLine());
         String[] balls = reader.readLine().split("");
-        int redBallCount = 0, blueBallCount = 0;
+        int redBallCountFromLeft = 0, redBallCountFromRight = 0;
+        int blueBallCountFromLeft = 0, blueBallCountFromRight = 0;
+        int minOfLeft = 0, minOfRight = 0;
         int ballCount = 0;
         boolean isRedNow = true;
-
         for (int i = 0; i < quantity; i++) {
             if (balls[i].equals("B")) {
                 if (isRedNow) {
                     isRedNow = false;
-                    redBallCount += ballCount;
+                    redBallCountFromLeft += ballCount;
                     ballCount = 0;
                 }
                 ballCount++;
             } else { 
                 if (!isRedNow) {
                     isRedNow = true;
-                    blueBallCount += ballCount;
+                    blueBallCountFromLeft += ballCount;
                     ballCount = 0;
                 }
                 ballCount++;
             }
         }
-        System.out.println(Math.min(redBallCount, blueBallCount));
+        minOfLeft = Math.min(redBallCountFromLeft, blueBallCountFromLeft);
+        
+        ballCount = 0;
+        isRedNow = true;
+        for (int i = quantity-1; i >= 0; i--) {
+            if (balls[i].equals("B")) { 
+                if (isRedNow) {
+                    isRedNow = false;
+                    redBallCountFromRight += ballCount;
+                    ballCount = 0;
+                }
+                ballCount++;
+            } else { 
+                if (!isRedNow) {
+                    isRedNow = true;
+                    blueBallCountFromRight += ballCount;
+                    ballCount = 0;
+                }
+                ballCount++;
+            }
+        }
+        minOfRight = Math.min(redBallCountFromRight, blueBallCountFromRight);
+
+        System.out.println(Math.min(minOfLeft, minOfRight));
+        
     }
 
 }
