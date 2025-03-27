46d6a0ce933cb3bfbca5b319ab6bd4f9f308ccec
DongGyu Jung <bang2brew@gmail.com>
2025-03-27 22:39:50 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.10 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long solution(int w, int h) {
+        long W = w, H = h; 
+        if (W == H) {
+            return W * H - W;
+        }
+        long gcd = getGCD(w, h);
+        return (W * H) - (W + H - gcd);
+    }
+    private long getGCD(long numA, long numB) {
+        long a = numA, b = numB;
+        long mod = Long.MAX_VALUE;
+        while (mod != 0) {
+            mod = a % b;
+            a = b;
+            b = mod;
+        }
+        return a;
+    }
+}
